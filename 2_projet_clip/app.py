361f24bc21d641198b4e849b17bda4f32b12d5d4
Erriep56 <pierre.husson.56@gmail.com>
2025-08-28 21:56:48 +0200

    modification du Readme.md, ajout des fichiers

---
+++ 2_projet_clip/app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import os
+import json
+import clip
+import torch
+from PIL import Image
+import numpy as np
+
+# Configuration
+st.set_page_config(layout="wide")
+images_folder = "images/4" # à modifier si besoin
+json_folder = "clip_json"
+
+# Charger le modèle CLIP
+@st.cache_resource
+def load_clip_model():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    return model, preprocess, device
+
+model, preprocess, device = load_clip_model()
+
+# Charger les embeddings
+@st.cache_data
+def load_embeddings():
+    embeddings = []
+    for json_file in os.listdir(json_folder):
+        if json_file.endswith(".json"):
+            with open(os.path.join(json_folder, json_file), "r", encoding="utf-8") as f:
+                data = json.load(f)
+                embeddings.append({
+                    "filename": data["filename"],
+                    "embedding": np.array(data["embedding"]).astype("float32")
+                })
+    return embeddings
+
+data = load_embeddings()
+
+# Interface
+st.title("🔬 Recherche visuelle avec CLIP 🧠")
+# Texte d'introduction
+st.text(
+    "🖼️ Bienvenue sur l'application Streamlit pour rechercher des images du RETIF grâce à CLIP ! 🖼️\n"
+    "🎇 Assurez-vous d'avoir téléchargé les images et préparé les embeddings avec CLIP. 🌄"
+)
+
+col1, col2 = st.columns(2)
+
+# Recherche textuelle
+with col1:
+    text_query = st.text_input("🔠 Recherche par mots-clés (ex : pyramid, dog...) 🔡")
+
+# Recherche par image
+with col2:
+    uploaded_image = st.file_uploader("📲 Ou uploadez une image pour chercher par contenu 🖥️", type=["jpg", "jpeg", "png"])
+
+# Résultats
+if not text_query and not uploaded_image:
+    st.info("🕵️‍♂️ Entrez un mot-clé ou uploadez une image pour lancer une recherche. 🕵️‍♀️")
+else:
+    if text_query:
+        with torch.no_grad():
+            tokens = clip.tokenize([text_query]).to(device)
+            text_features = model.encode_text(tokens)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            query_vector = text_features.cpu().numpy()[0]
+        st.subheader(f"Résultats pour : *{text_query}*")
+
+    elif uploaded_image:
+        try:
+            pil_img = Image.open(uploaded_image).convert("RGB")
+            # Affichage dans la colonne 2
+            with col2:
+                st.image(pil_img, caption="Image envoyée", width='stretch')
+            image_input = preprocess(pil_img).unsqueeze(0).to(device)
+            with torch.no_grad():
+                image_features = model.encode_image(image_input)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                query_vector = image_features.cpu().numpy()[0]
+            st.subheader("Résultats similaires à l’image")
+        except Exception as e:
+            st.error(f"Erreur avec l’image envoyée : {e}")
+            st.stop()
+
+    # Recherche par similarité
+    results = []
+    for item in data:
+        score = np.dot(query_vector, item["embedding"])
+        results.append((score, item["filename"]))
+
+    # Trier et afficher
+    results.sort(reverse=True)
+    cols = st.columns(5)
+    for idx, (score, filename) in enumerate(results[:50]):
+        with cols[idx % 5]:
+            st.image(os.path.join(images_folder, filename), caption=f"{filename}\nScore: {score:.2f}", width='stretch')
---
+++ README.md
@@ -0,0 +1,95 @@
+# 📦 Livrables du stage M2 TNAH - INHA, PictorIA
+
+Ce dépôt contient les livrables réalisés dans le cadre de mon stage de Master 2 *Technologies Numériques Appliquées à l’Histoire* (TNAH) à l'INHA, en collaboration avec le consortium Huma-Num PictorIA, entre avril et juillet 2025.
+
+---
+
+## 📁 Contenu du dépôt
+
+### 1. 🧠 Conversion du thésaurus Garnier (XML-RDF → JSON & TXT)
+
+Le dossier `1_conversion_garnier` contient un script Python permettant de convertir le fichier XML-RDF du **thésaurus Garnier (INHA)** en deux formats plus facilement exploitables : JSON et TXT.
+
+Le fichier XML-RDF du thesaurus Garnier (INHA) est déjà contenu dans le dépôt.
+
+#### ⚙️ Prérequis
+
+Avant d'exécuter le script, installez la librairie `rdflib` :
+
+```bash 
+pip install rdflib
+```
+
+ou
+
+```bash
+cd 1_conversion_garnier
+pip install -r requirements.txt
+```
+
+
+#### 📄 Fichiers générés
+
+Deux fichiers sont ainsi générés :
+
+- **JSON** : une représentation hiérarchique complète du thésaurus. Pour chaque concept, sont inclus :
+  - L'URI (`id`)
+  - Le label principal (`prefLabel`)
+  - Les labels alternatifs (`altLabels`)
+  - Les enfants (`children`)
+
+  Les concepts sont structurés sous forme de dictionnaires imbriqués, triés par ordre alphabétique.
+
+- **TXT** : une version textuelle du thésaurus, avec uniquement les labels principaux (`prefLabel`). La hiérarchie est visualisée grâce à une indentation par niveau.
+
+---
+
+### 2. 🖼️ Téléchargement des images du RETIF & Application Streamlit avec CLIP
+
+Le dossier `2_projet_clip` contient :
+- **Deux notebooks Jupyter** :
+  - Le **notebook 1** (`1_recup_images_AGORHA.ipynb`) permet le téléchargement des images du RETIF via l’API d’AGORHA
+  - Le **notebook 2** `2_embeddings_CLIP.ipynb` sert à générer les embeddings des images à l’aide du de la technologie CLIP
+  
+- Un fichier python `app.py` permettant de lancer l'**application Streamlit** pour la recherche visuelle d’images à partir de texte et d'images, en s’appuyant sur les embeddings CLIP générés par le Notebook 2.
+
+
+#### 🏃‍♀️ Lancer l'application Streamlit
+
+Pour lancer l'application Streamlit, il faut d'abord télécharger les images avec le **Notebook 1** puis générer les embeddings avec le **Notebook 2**.
+
+Ensuite, installer les `requirements_streamlit.txt` depuis le dossier `2_recup_images_CLIP`(ils peuvent être également installés avant l'utilisation des Jupyter Notebooks) :
+
+```bash
+pip install requirements_streamlit.txt
+```
+
+Pour lancer l'application, écrivez la commande suivante dans le terminal, depuis le dossier `2_projet_clip` :
+
+```bash
+streamlit run app.py
+```
+
+
+---
+
+### 3. 📗 Protocole : Indexation avec Panoptic
+
+Un fichier PDF décrivant **le protocole d’utilisation de Panoptic** pour indexer les images du RETIF à l'aide du thesaurus Garnier. Ce document sert de guide pour reproduire l’indexation ou adapter le processus à d'autres corpus.
+
+---
+
+## 🧪 Environnement conseillé
+
+- **Python 3.9 à 3.12+**
+- Librairies nécessaires : 
+  - `rdflib`
+  - `streamlit`
+  - `git+https://github.com/openai/CLIP.git`
+  - `numpy==1.26.1`
+
+---
+
+## 📑 Licence
+
+Ce projet est sous licence MIT. Consultez le fichier `LICENSE` pour plus de détails.
